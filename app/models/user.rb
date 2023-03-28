88caf872ac1c8416af8645d30ade550decb8476e
Kevin <kelvinngechu@gmail.com>
2023-03-28 21:12:57 +0300

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    has_many :orders
+    has_many :order_items, through: :orders
+
+    validates :first_name, :last_name, :email, presence: true
+    validates :email, uniqueness: true
+    # validates :password, length: { minimum: 2 }
+end
