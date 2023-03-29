a8b7ec6c46b82c3e732eaff974526b6e70a76b5d
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-03-29 08:15:43 +0300

    added name column to items and password_reset_token and password_reset_sent_at for password reset functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_105623) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_051320) do
   create_table "items", force: :cascade do |t|
     t.string "description", null: false
     t.integer "price", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_105623) do
     t.string "imgURL", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -60,6 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_105623) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
   add_foreign_key "order_items", "items"
