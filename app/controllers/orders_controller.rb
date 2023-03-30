5c7347f23b9f96426b9e4a3908c4ed074a391fbe
Kevin <kelvinngechu@gmail.com>
2023-03-30 15:35:10 +0300

    conf

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,34 @@
+# frozen_string_literal: true
+
 class OrdersController < ApplicationController
-end
+    def index
+      @orders = Order.all
+    end
+  
+    def show
+      @order = Order.find(params[:id])
+    end
+  
+    def create
+      @order = Order.new(order_params)
+  
+      if @order.save
+        redirect_to @order, notice: 'Order was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def destroy
+      @order = Order.find(params[:id])
+      @order.destroy
+      redirect_to orders_url, notice: 'Order was successfully destroyed.'
+    end
+  
+    private
+  
+    def order_params
+      params.require(:order).permit(:user_id, :item_id)
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
+
+  resources :payments, only: [:new, :create]
+  
   resources :user_names
-  resources :payments
+  #resources :payments
   resources :order_items
   resources :orders
   resources :items, only: [:index, :show]
   resources :users
-  get 'password/reset', to:'password_reset#new'
-  post 'password/reset', to:'password_reset'
+ 
 
   post 'users/login', to: 'users#login'
   post 'users/signup', to: 'users#signup'
+
+ 
+
+ 
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
