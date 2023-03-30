494e179755ab803a786a815c1e67215f55c448e2
Kevin <kelvinngechu@gmail.com>
2023-03-30 12:58:26 +0300

    creating password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :user_names
   resources :payments
   resources :order_items
   resources :orders
   resources :items
   resources :users
+  get 'password/reset', to:'password_reset#new'
+  post 'password/reset', to:'password_reset'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,60 +1,24 @@
-puts 'Seeding...'
+# frozen_string_literal: true
 
+Item.destroy_all
 
-2.times do
-  User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: 'password123'
-  )
-end
+# appetizers
+Item.create(name: 'Suya Skewers', description: 'Grilled meat skewers coated in spicy peanut sauce', price: 12, category: 'appetizers', imgURL: 'https://cheflolaskitchen.com/wp-content/uploads/2023/01/nigerian-suya-32-683x1024.jpg')
+Item.create(name: 'Puff Puff', description: 'Deep-fried dough balls served with a spicy dipping sauce', price: 8, category: 'appetizers', imgURL: 'https://keeshaskitchen.com/wp-content/uploads/2022/02/20220107091914-puff-20puff-20beignets.jpg')
+Item.create(name: 'Akara Fritters', description: 'Deep-fried black-eyed pea fritters served with a tomato and onion sauce', price: 10, category: 'appetizers', imgURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Acaraje-Baiana.jpg/1200px-Acaraje-Baiana.jpg')
+Item.create(name: 'Biltong', description: 'Air-dried meat slices seasoned with spices and served with crackers', price: 14, category: 'appetizers', imgURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsK6DUgN9Ei87q6f9z1qrXcmoqTBZSLbvnVA&usqp=CAU')
+Item.create(name: 'Samosas', description: 'Crispy pastry pockets filled with spiced vegetables or meat, served with chutney', price: 9, category: 'appetizers', imgURL: 'https://www.recipetineats.com/wp-content/uploads/2021/02/Samosas-on-plate-SQ.jpg')
 
+# main
+Item.create(name: 'Jollof Rice', description: 'Spiced rice cooked with tomatoes, onions, and peppers, served with your choice of meat or vegetables', price: 16, category: 'main', imgURL: 'https://eatwellabi.com/wp-content/uploads/2022/11/Jollof-rice-16.jpg')
+Item.create(name: 'Fufu and Soup', description: 'Mashed cassava or yam served with a savory soup made with meat or vegetables', price: 18, category: 'main', imgURL: 'https://www.africanbites.com/wp-content/uploads/2022/02/a-bowl-of-okra-stew-copy.jpg')
+Item.create(name: 'Doro Wat', description: 'Ethiopian spiced chicken stew served with injera bread', price: 20, category: 'main', imgURL: 'https://glebekitchen.com/wp-content/uploads/2020/01/dorowatscenetop.jpg')
+Item.create(name: 'Bobotie', description: 'South African curried meat dish with a creamy topping of egg custard and served with yellow rice', price: 22, category: 'main', imgURL: 'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/rx063_beef_mince_4_of_6.jpg')
+Item.create(name: 'Suya Burger', description: 'Grilled beef patty seasoned with spicy peanut sauce, topped with caramelized onions and served on a bun', price: 15, category: 'main', imgURL: 'https://media-cdn.tripadvisor.com/media/photo-s/1c/2d/5f/fd/our-signature-suya-burger.jpg')
+Item.create(name: 'Vegetable Tagine', description: 'Moroccan stew made with chickpeas, sweet potatoes, and vegetables, served with cousc', price: 12, category: 'main', imgURL: 'https://www.deliciousmagazine.co.uk/wp-content/uploads/2018/07/919361-1-eng-GB_vegetable-tagine-768x960.jpg')
 
-5.times do
-  Item.create!(
-    description: Faker::Food.dish,
-    price: Faker::Number.decimal(l_digits: 2),
-    category: Faker::Food.ethnic_category,
-    imgURL: Faker::LoremFlickr.image(size: "300x300", search_terms: ['food'])
-  )
-end
-
-
-5.times do
-  Order.create!(
-    user_id: rand(1..2),
-    item_id: rand(1..5)
-  )
-end
-
-
-5.times do
-  quantity = rand(1..5)
-  item = Item.find(rand(1..5))
-  subtotal = quantity * item.price
-
-  OrderItem.create!(
-    order_id: rand(1..5),
-    item_id: item.id,
-    quantity: quantity,
-    subtotal: subtotal
-  )
-end
-
-
-5.times do
-  Payment.create!(
-    order_id: rand(1..5),
-    amount: Faker::Number.decimal(l_digits: 2),
-    payment_method: Faker::Business.credit_card_type,
-    card_number: Faker::Business.credit_card_number,
-    cardholder_name: Faker::Name.name,
-    expiration_date: Faker::Business.credit_card_expiry_date,
-    cvc: Faker::Number.number(digits: 3)
-  )
-end
-
-  
-  puts 'Done seeding!'
-  
+# drinks
+Item.create(name: 'Bissap Juice', description: 'Tart and refreshing hibiscus flower juice', price: 4, category: 'drinks', imgURL: 'https://tarasmulticulturaltable.com/wp-content/uploads/2022/10/Bissap-2-of-3.jpg')
+Item.create(name: 'Palm Wine', description: 'Sweet and mildly alcoholic beverage made from the sap of various species of palm trees', price: 6, category: 'drinks', imgURL: 'https://ocdn.eu/pulscms-transforms/1/63Ak9kpTURBXy9hN2Y5ZmMyZDA0NTM2YjZlNjQ3MDFhYzhlZDkyYjY5YS5qcGeSlQMAAs0CvM0BiZMFzQJYzQE73gABoTAF')
+Item.create(name: 'Ginger Beer', description: 'Fizzy and spicy non-alcoholic drink made with fresh ginger root', price: 3, category: 'drinks', imgURL: 'https://www.oculyze.net/wp-content/uploads/2022/03/does-ginger-beer-have-alcohol-in-it-4.jpg')
+Item.create(name: 'Mango Lassi', description: 'Sweet and creamy yogurt-based drink with mango and spices', price: 5, category: 'drinks', imgURL: 'https://www.teaforturmeric.com/wp-content/uploads/2021/07/Mango-Lassi-Recipe-Card-1.jpg')
