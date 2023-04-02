10bc5237790015fabd4c6d170c10614cd9c16415
Lawrence Kimani <lawrencekimani18@gmail.com>
2023-04-02 19:25:36 +0300

    Added CORS configuration to allow requests from frontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,39 @@ class UsersController < ApplicationController
         render json: user, status: :ok
     end
 
+    def password_reset
+        user = User.find_by_email(params[:email])
+        if user
+          user.generate_password_reset_token
+          user.save!
+          UserMailer.password_reset(user).deliver_now
+          render json: { message: "Password reset instructions sent to #{user.email}" }, status: :ok
+        else
+          render json: { error: "User with email #{params[:email]} not found" }, status: :not_found
+        end
+    end
 
+    def reset_password
+        @user = User.find_by_password_reset_token(params[:token])
+        if @user && @user.password_reset_sent_at > 2.hours.ago
+          # Render the reset password form
+        else
+          render json: { error: "Invalid or expired reset password link" }, status: :unprocessable_entity
+        end
+    end
 
-
+    def update_password
+        @user = User.find_by_password_reset_token(params[:token])
+        if @user && @user.password_reset_sent_at > 2.hours.ago
+          if @user.update(password: params[:password])
+            render json: { message: "Password updated successfully" }, status: :ok
+          else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { error: "Invalid or expired reset password link" }, status: :unprocessable_entity
+        end
+    end
 
     private
 
